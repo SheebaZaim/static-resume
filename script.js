3653f7b3794b10baa8a971bd016c2834e8f4bb05
SheebaZaim <160307093+SheebaZaim@users.noreply.github.com>
2024-09-15 00:52:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var Button = document.getElementById('toggle-skills');
+var Skills = document.getElementById('skills');
+Button.addEventListener('click', function () {
+    if (Skills.style.display === 'none') {
+        Skills.style.display = 'block';
+    }
+    else {
+        Skills.style.display === 'none';
+    }
+});
